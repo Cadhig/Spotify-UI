da901e9846e9089352c7fe1e55e3d6b31b8bac25
Cadence K <cadencehiggins@gmail.com>
2024-06-23 17:16:51 -0500

    Styled SideBar.tsx
    styled sidebar with hardcoded default values

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,54 @@
 import React from 'react'
+import { House, Search } from 'lucide-react'
 
 export default function SideBar() {
+    return (
+        <div className='bg-black w-1/6 h-full'>
+            <HomeAndSearch />
+            <PlaylistList />
+        </div>
+    )
+}
+
+export function HomeAndSearch() {
+    return (
+        <div className='text-gray-300 font-bold bg-zinc-900 rounded-lg h-36 flex flex-col justify-evenly pl-4 m-2'>
+            <div className='flex flex-row items-center gap-3 hover:text-white cursor-pointer'>
+                <House size={36} />
+                <p>Home</p>
+            </div>
+            <div className='flex flex-row items-center gap-3 hover:text-white cursor-pointer'>
+                <Search size={36} />
+                <p>Search</p>
+            </div>
+        </div>
+    )
+}
+
+export function PlaylistList() {
 
     return (
-        <div className='bg-green-500 w-1/6 h-full'>
-            <p>sidebar</p>
+        <div className='text-white bg-zinc-900 rounded-lg h-4/5 flex flex-col gap-4 pl-4 pt-4 m-2 overflow-auto'>
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+            <Playlist />
+
+
+        </div>
+    )
+}
+
+export function Playlist() {
+
+    return (
+        <div className='flex items-center gap-3'>
+            <img src="src\assets\Placeholder.jpg" alt="palceholder" className='w-14 h-14 rounded' />
+            <div className='flex flex-col'>
+                <p className='font-bold'>Playlist Title</p>
+                <p className='text-gray-400'>User</p>
+            </div>
         </div>
     )
 }
