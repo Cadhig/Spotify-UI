8252ec4fa0c28999c83f5dbb57eafda863e633da
Cadence K <cadencehiggins@gmail.com>
2024-06-23 16:24:04 -0500

    Styled BottomBar.tsx
    Completed base styling, hardcoded placeholder components, reactive play button

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import { Shuffle, SkipBack, CirclePlay, SkipForward, Repeat, Volume2, CirclePause } from 'lucide-react'
 
 export default function SideBar() {
 
+    const [paused, setPaused] = React.useState(true)
+
+    let playState;
+
+    if (paused === true) {
+        playState = <CirclePlay size={48} />
+    } else {
+        playState = <CirclePause size={48} />
+    }
     return (
-        <div className='bg-lime-600 w-full h-20'>
-            <p>BottomBar</p>
+        <div className='bg-black w-full h-20'>
+            <div className='flex m-3 items-center'>
+                <div className='flex justify-start items-center w-1/4'>
+                    <img src="src\assets\Placeholder.jpg" alt="" className='w-16 h-14' />
+                    <div className='ml-3 text-white'>
+                        <p>Song Name</p>
+                        <p className='text-gray-500'>Artist</p>
+                    </div>
+                </div>
+                <div className='w-1/2 flex flex-col justify-center items-center gap-1 h-full text-lg'>
+                    <div className='text-white flex gap-2 justify-center items-center h-full'>
+                        <Shuffle size={16} />
+                        <SkipBack size={32} />
+                        <button onClick={() => {
+                            if (paused === true) {
+                                setPaused(false)
+                            } else {
+                                setPaused(true)
+                            }
+                        }}>{playState}</button>
+                        <SkipForward size={32} />
+                        <Repeat size={16} />
+                    </div>
+                    <div id='MockBar' className='bg-white w-1/2 h-1'>
+                    </div>
+                </div>
+                <div className='text-white flex items-center gap-3 w-1/4 justify-end'>
+                    <Volume2 />
+                    <div id='MockVol' className='bg-white w-28 h-1'>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SideBar() {
 
     return (
-        <div className='bg-black w-1/6 h-full'>
+        <div className='bg-green-500 w-1/6 h-full'>
             <p>sidebar</p>
         </div>
     )
